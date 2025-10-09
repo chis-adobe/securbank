9547f011e81263280ec3bf8c73df054a766b5e46
Liviu Chis <chis@adobe.com>
2025-10-09 11:50:58 -0700

    Adjusted envs

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p130746-e1275972.adobeaemcloud.com';
+  return 'https://publish-p130746-e1298459.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p130746-e1275972.adobeaemcloud.com';
+  return 'https://author-p130746-e1298459.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
