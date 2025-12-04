43c4a7bfd2235fe2be1886e1a278f137048349ee
Liviu Chis <chis@adobe.com>
2025-12-04 11:59:09 -0800

    Added google tag

--- blocks/rbc-offer/rbc-offer.js
+++ blocks/rbc-offer/rbc-offer.js
@@ -35,6 +35,12 @@ export default async function decorate(block) {
   const mobileImageUrl = `https://s7d1.scene7.com/is/image/LiviuChisNA001/${filenameWithoutExt}:Small`;
   const desktopImageUrl = aempublishurl + cfReq.heroImage._dynamicUrl;
 
+  // Generate tag ID for Google Analytics
+  const pageName = document.title || window.location.pathname.split('/').filter(Boolean).pop() || 'home';
+  const blockName = 'rbc-offer';
+  const ctaLabel = (cfReq.callToAction || 'View Offer Details').toLowerCase().replace(/\s+/g, '-');
+  const tagId = `${pageName.toLowerCase().replace(/\s+/g, '-')}_${blockName}_${ctaLabel}`;
+
   block.innerHTML = `
   <div class='rbc-offer-container' data-aue-resource=${itemId} data-aue-label="offer content fragment" data-aue-type="reference" data-aue-filter="cf">
       <div class='rbc-offer-background' data-mobile-bg="${mobileImageUrl}" data-desktop-bg="${desktopImageUrl}">
@@ -43,7 +49,7 @@ export default async function decorate(block) {
           <div data-aue-prop="pretitle" data-aue-label="pretitle" data-aue-type="text" class='rbc-offer-pretitle'>${cfReq.pretitle}</div>
           <h2 data-aue-prop="headline" data-aue-label="headline" data-aue-type="text" class='rbc-offer-headline'>${cfReq.headline}</h2>
           <p data-aue-prop="detail" data-aue-label="detail" data-aue-type="richtext" class='rbc-offer-detail'>${cfReq.detail.plaintext}</p>
-          <a href="#" data-aue-prop="callToAction" data-aue-label="callToAction" data-aue-type="text" class='rbc-offer-cta'>${cfReq.callToAction || 'View Offer Details'}</a>
+          <a href="#" data-aue-prop="callToAction" data-aue-label="callToAction" data-aue-type="text" class='rbc-offer-cta' data-tag-id="${tagId}">${cfReq.callToAction || 'View Offer Details'}</a>
           <div data-aue-prop="conditions" data-aue-label="conditions" data-aue-type="text" class='rbc-offer-conditions'>
             <svg class="rbc-offer-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" fill="currentColor"/>
