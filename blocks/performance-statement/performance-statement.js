ec6d359588f53cc17c2e4c466f2137fb85f4d7e8
Liviu Chis <chis@adobe.com>
2026-03-06 09:28:29 -0800

    Added performance statement

---
+++ blocks/performance-statement/performance-statement.js
@@ -0,0 +1,33 @@
+import { getAEMPublish, getAEMAuthor } from '../../scripts/endpointconfig.js';
+
+export default async function decorate(block) {
+  const aempublishurl = getAEMPublish();
+  const aemauthorurl = getAEMAuthor();
+  const persistedquery = '/graphql/execute.json/securbank/MetricByPath';
+  const fragmentPath = block.querySelector(':scope div:nth-child(1) > div a')?.innerHTML?.trim() || '';
+  if (!fragmentPath) return;
+
+  const url = window.location?.origin?.includes('author')
+    ? `${aemauthorurl}${persistedquery};path=${fragmentPath};ts=${Math.random() * 1000}`
+    : `${aempublishurl}${persistedquery};path=${fragmentPath};ts=${Math.random() * 1000}`;
+  const options = { credentials: 'include' };
+
+  const data = await fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => json?.data?.performanceMetricByPath?.item || null);
+
+  if (!data) return;
+
+  const itemId = `urn:aemconnection:${fragmentPath}/jcr:content/data/master`;
+  const title = data.title || '';
+  const dollar = data.dollar || '';
+  const percent = data.percent || '';
+  const dollarDisplay = dollar.startsWith('$') ? dollar : `$${dollar}`;
+  const sentence = `Grew ${title} by ${dollarDisplay} which represents ${percent} improvement`;
+
+  block.innerHTML = `
+  <div class="performance-statement-wrapper" data-aue-resource="${itemId}" data-aue-label="performance statement content fragment" data-aue-type="reference" data-aue-filter="cf">
+    <p class="performance-statement-text" data-aue-prop="statement" data-aue-label="Statement" data-aue-type="text">${sentence}</p>
+  </div>
+  `;
+}
